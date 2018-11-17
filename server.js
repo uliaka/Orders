2d1e37e8590d45c91c0c6f97b415b6386712f2ec
uliaka <uliana.gorin95@gmail.com>
2018-11-17 17:37:20 +0200

    create Post endpoint

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 mongoose.connect('mongodb://localhost:27017/test');
 
@@ -13,16 +14,23 @@ db.on('error', function(err) {
 db.once('open', function() {
   console.log('connection is open')
 
-//  const newOrder = new Order({ title: 'awesome order 2', price: 30 });
-//  newOrder.save();
+ //const newOrder = new Order({ title: 'awesome order 2', price: 30 });
+  //newOrder.save();
 
 });
+
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
   res.sendFile('public/index.html');
 });
 
+app.post('/orders', function (req, res) {
+  const newOrder = new Order({ title: req.body.title, price: req.body.price });
+  newOrder.save();
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
