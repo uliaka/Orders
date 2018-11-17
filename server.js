82ae852a72046edf71004295ead1568cd15a4ac8
uliaka <uliana.gorin95@gmail.com>
2018-11-17 16:50:24 +0200

    connect to mongo

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost:27017/test');
+var db = mongoose.connection;
+db.on('error', function(err) {
+  console.log(err)
+});
+db.once('open', function() {
+  console.log('connection is open')
+});
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
