dbb96a19fcbb02a9f0ae71005fe320921d3312e8
michaelost <mikhaylo.ostroviy@gmail.com>
2018-11-17 16:04:50 +0200

    add html file

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile('public/index.html');
 });
 
 app.listen(3000, function () {
