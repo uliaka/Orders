8c12255be128ee20143d58639888e236a7d7e514
michaelost <mikhaylo.ostroviy@gmail.com>
2018-11-20 20:58:47 +0200

    use passportjs

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
+const flash = require('connect-flash');
+
+const passport = require('passport');
+const Strategy = require('passport-local').Strategy;
+
+const session = require('express-session')
 
 mongoose.connect('mongodb://localhost:27017/test');
 
@@ -12,10 +18,6 @@ db.on('error', function(err) {
   console.log(err)
 });
 db.once('open', function() {
-  console.log('connection is open')
-
- //const newOrder = new Order({ title: 'awesome order 2', price: 30 });
-  //newOrder.save();
 
 });
 
@@ -23,12 +25,30 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(flash())
+
+
+app.use(session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new Strategy(
+  function(username, password, done) {
+    if (username !== 'michaelost') {
+     return done(null, false);
+    }
+
+     return done(null, {});
+  }
+));
+
 app.get('/', function (req, res) {
   res.sendFile('public/index.html');
   res.end();
 });
 
-app.post('/orders', function (req, res) {
+app.post('/orders',  function (req, res) {
   const newOrder = new Order({ title: req.body.title, price: req.body.price });
   newOrder.save();
   res.sendFile(__dirname + '/index.html');
@@ -41,6 +61,24 @@ app.get('/orders', function (req, res) {
     res.end();
   });
 });
+
+
+
+app.get('/login', function (req, res) {
+  const orders = Order.find({}).then(function (data) {
+    res.send({data: 'login page'});
+    res.end();
+  });
+});
+
+const passportConfig = { session: false,  successRedirect: '/', failureRedirect: '/login' };
+
+app.post('/login', passport.authenticate('local', passportConfig), function (req, res) {
+  console.log('login handler function')
+  res.json({opa: 5})
+
+});
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
